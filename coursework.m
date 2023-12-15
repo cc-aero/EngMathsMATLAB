8cca0a99ecd805a65f4f1aadad4a50bd8efe7e2e
cc-aero <74114283+cc-aero@users.noreply.github.com>
2023-12-15 18:31:27 +0000

    checkpoint 3 works but needs adapting to infinite bounds, not small area

--- coursework.m
+++ coursework.m
@@ -18,8 +18,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-res=100;
-x_rng = linspace(-1,1,res);
+
 %p = pdf('Normal',x_rng,0,0.5); %This is NOT the proper way to do it;
                                 %this creates a discrete function, which 
                                 %we cannot integrate. We need continuous.
@@ -27,20 +26,9 @@ x_rng = linspace(-1,1,res);
 p = @(x) normpdf(x,0,0.4);  %Here is a continuous PDF for testing purposes
 
 
-calcProbs(p,-0.35,-0.3,0,0.4)
+calcProbs(p,-0.35,-0.3,0.1,0.4,0.1,0.3);
 
-function[prob, mean_val, one_std,two_std] = calcProbs(pd_func,range_min,range_max, mu1,sigma1)
-    nDist = @(x) normpdf(x,mu1,sigma1);
-    res = 100;
-    xvals = linspace(-5*sigma1,5*sigma1,res)
-    dvals = nDist(xvals)
-    plot(xvals,dvals)
-    xlabel("x")
-    ylabel("Probability Density")
-    
-    mean_val = nDist(mu1);
-    one_std = nDist(mu1 + sigma1);
-    two_std = nDist(mu1 + 2*sigma1);
+function[prob, mean_val, one_std,two_std,area] = calcProbs(pd_func,range_min,range_max, mu1,sigma1, mu2,sigma2)
     %{
 Brief:      Ascertains probability of data lying in range of a PDF
 Details:    //!TODO long details
@@ -62,8 +50,32 @@ Imports:
     - prob      |   float   |   Probability of value being within interval
         
     %}
+res = 1000;     %1000 by default. x axis resolution
+%PART 1
 prob=integral(pd_func,range_min,range_max);
+
+%PART 2
+    nDist = @(x) normpdf(x,mu1,sigma1);
+    xvals = linspace(-5*sigma1,5*sigma1,res);    %Values for x axis
+    dvals = nDist(xvals);    %Normal distribution values
+    %plot(xvals,dvals)
+
+    %xlabel("x")
+    %ylabel("Probability Density")
+
+    mean_val = nDist(mu1);
+    one_std = nDist(mu1 + sigma1);
+    two_std = nDist(mu1 + 2*sigma1);
+
+%PART 3
+    nDist2 = @(x) normpdf(x,mu2,sigma2);
+    xvals2 = linspace(-5*sigma2,5*sigma2,res);    %Values for x axis
+    dvals2 = nDist(xvals2);    %Normal distribution values
     
+    
+    area = integral((@(x) min(nDist(x), nDist2(x))),-inf,inf);
+    
+    area
 end
 
 
